142ce64f47cda62b64edc152a7ce67df2cb1657b
kanagaraj <kanagaraj@nanonino.in>
2023-11-02 16:37:03 +0530

    app name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>This is kanagaraj</h1>
+        <h2>netlify web app deploying of free</h2>
       </header>
     </div>
   );
